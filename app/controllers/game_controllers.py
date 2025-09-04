88140f863e4d817de53d679ba9abcf7c103b5540
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-09-04 11:22:54 +0200

    Refactor game_service to handle caching

--- app/controllers/game_controllers.py
+++ app/controllers/game_controllers.py
@@ -1,5 +1,4 @@
-from typing import cast
-from flask import Flask, current_app, jsonify, Response, request
+from flask import Flask, jsonify, Response, request
 from flask_jwt_extended import jwt_required, current_user  # pyright: ignore[reportUnknownVariableType]
 from app.services.dtos import PlayerMoveDto
 from app.services import game_service
@@ -11,31 +10,27 @@ def init_game_endpoints(app: Flask):
     @app.route("/api/game", methods=["POST"])
     @jwt_required()
     def create_game():  # pyright: ignore[reportUnusedFunction]
-        game_sessions = cast(Flask, current_app).game_sessions
-        game_service.create_game(current_user, game_sessions)
+        game_service.create_game(current_user)
         return Response(status=201)
 
     @app.route("/api/game/active", methods=["GET"])
     @jwt_required()
     def check_active_game_status():  # pyright: ignore[reportUnusedFunction]
-        game_sessions = cast(Flask, current_app).game_sessions
-        active_status = game_service.check_active_game(current_user, game_sessions)
+        active_status = game_service.check_active_game(current_user)
 
         return jsonify({"status": active_status}), 200
 
     @app.route("/api/game", methods=["GET"])
     @jwt_required()
     def get_current_game():  # pyright: ignore[reportUnusedFunction]
-        game_sessions = cast(Flask, current_app).game_sessions
-        game_state_dto = game_service.get_active_game(current_user, game_sessions)
+        game_state_dto = game_service.get_active_game(current_user)
         return jsonify(asdict(game_state_dto)), 200
 
     @app.route("/api/game", methods=["PATCH"])
     @jwt_required()
     def make_move():  # pyright: ignore[reportUnusedFunction]
-        game_sessions = cast(Flask, current_app).game_sessions
         body = request.get_json()
         player_move = PlayerMoveDto(**body)
 
-        game_state_dto = game_service.make_player_move(current_user, game_sessions, player_move)
+        game_state_dto = game_service.make_player_move(current_user, player_move)
         return jsonify(asdict(game_state_dto)), 200
--- app/services/game_service.py
+++ app/services/game_service.py
@@ -1,35 +1,36 @@
 from app.game.gameplay import handle_player_step
 from app.game.models import ActionType, Coordinates, Game, GameState
-from app.services.game_session_manager import GameSessionManager
 from app.game.generation import generate_game
 from .dtos import GameDto, PlayerMoveDto
+from app.extensions import cache
 
 
-def create_game(user_id: int, game_sessions: GameSessionManager):
+def create_game(user_id: int):
     game: Game = generate_game()
-    game_sessions.add_game(user_id, game)
+    cache.set(user_id, game)
 
 
-def check_active_game(user_id: int, game_sessions: GameSessionManager) -> bool:
-    game = game_sessions.get_game(user_id)
-    return True if game else False
+def check_active_game(user_id: int) -> bool:
+    return cache.has(user_id)
 
 
-def get_active_game(user_id: int, game_sessions: GameSessionManager) -> GameDto | None:
-    game = game_sessions.get_game(user_id)
+def get_active_game(user_id: int) -> GameDto | None:
+    game = cache.get(user_id)
     if not game:
         return None
     return GameDto.from_game(game)
 
 
-def make_player_move(user_id: int, game_sessions: GameSessionManager, player_move: PlayerMoveDto) -> GameDto | None:
-    game = game_sessions.get_game(user_id)
+def make_player_move(user_id: int, player_move: PlayerMoveDto) -> GameDto | None:
+    game = cache.get(user_id)
     action_type = ActionType[player_move.action_type]
     action_coordinates = Coordinates(**player_move.coordinates)
 
     if game:
         handle_player_step(game, action_type, action_coordinates)
         if game.state == GameState.FINISHED_LOST or game.state == GameState.FINISHED_WON:
-            game_sessions.remove_game(user_id)
+            cache.delete(user_id)
+        else:
+            cache.set(user_id, game)
         return GameDto.from_game(game)
     return None
