5193678d48a48c48da0a48c2cea06777115562d4
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-09-09 09:48:47 +0200

    Implement database error handling

--- app/error_handling/error_handlers.py
+++ app/error_handling/error_handlers.py
@@ -3,16 +3,35 @@ from pydantic import ValidationError
 from werkzeug.exceptions import HTTPException
 from app.dto.exception_dto import ExceptionDto
 from app.error_handling.exceptions import ApiException
+from sqlalchemy.exc import OperationalError, InterfaceError, InternalError, IntegrityError, DataError, ProgrammingError, SQLAlchemyError
 
 
 def init_error_handlers(app: Flask):
+
+    @app.errorhandler(OperationalError)
+    @app.errorhandler(InterfaceError)
+    @app.errorhandler(InternalError)
+    def handle_database_connection_error(exception: OperationalError | InterfaceError):  # pyright: ignore[reportUnusedFunction]
+        return jsonify([ExceptionDto(code="DATABASE_CONNECTION_FAILED", message=str(exception.__cause__))]), 500
+
+    @app.errorhandler(IntegrityError)
+    @app.errorhandler(DataError)
+    @app.errorhandler(ProgrammingError)
+    def handle_database_query_error(exception: IntegrityError | DataError | ProgrammingError):  # pyright: ignore[reportUnusedFunction]
+        return jsonify([ExceptionDto(code="DATABASE_QUERY_FAILED", message=str(exception.__cause__))]), 500
+
+    @app.errorhandler(SQLAlchemyError)
+    def handle_database_error(exception: SQLAlchemyError):  # pyright: ignore[reportUnusedFunction]
+        return jsonify([ExceptionDto(code="DATABASE_ERROR", message=str(exception.__cause__))]), 500
 
     @app.errorhandler(ValidationError)
     def handle_validation_error(exception: ValidationError):  # pyright: ignore[reportUnusedFunction]
         errors = exception.errors()
         error_logs: list[ExceptionDto] = []
         for error in errors:
-            error_logs.append(ExceptionDto(code=error["type"], message=f"{"->".join(map(str, error["loc"]))}: {error["msg"]}"))
+            error_logs.append(ExceptionDto(
+                code=error["type"],
+                message=f"{"->".join(map(str, error["loc"]))}: {error["msg"]}"))
         return jsonify(error_logs), 400
 
     @app.errorhandler(ApiException)
@@ -25,4 +44,7 @@ def init_error_handlers(app: Flask):
 def http_exception_handler(exception: HTTPException):
     response_code = exception.code if exception.code else 500
     description = exception.description if exception.description else "Internal Server Error"
-    return jsonify(ExceptionDto(code=f"HTTP_{type(exception).__name__}", message=description)), response_code
+    return (
+        jsonify([ExceptionDto(code=f"HTTP_{type(exception).__name__}", message=description).model_dump()]),
+        response_code
+        )
