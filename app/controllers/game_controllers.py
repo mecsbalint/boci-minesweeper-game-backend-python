2776d0d7d17dca65a671938d01a6a9b353d9e602
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-09-10 12:21:02 +0200

    Bugfix where current_user is passed to sqlalchemy without unwrapping it's content

--- app/controllers/game_controllers.py
+++ app/controllers/game_controllers.py
@@ -10,20 +10,20 @@ def init_game_endpoints(app: Flask):
     @app.route("/api/game", methods=["POST"])
     @jwt_required()
     def create_game():  # pyright: ignore[reportUnusedFunction]
-        game_service.create_game(current_user)
+        game_service.create_game(current_user._get_current_object())
         return Response(status=201)
 
     @app.route("/api/game/active", methods=["GET"])
     @jwt_required()
     def check_active_game_status():  # pyright: ignore[reportUnusedFunction]
-        active_status = game_service.check_active_game(current_user)
+        active_status = game_service.check_active_game(current_user._get_current_object())
 
         return jsonify({"status": active_status}), 200
 
     @app.route("/api/game", methods=["GET"])
     @jwt_required()
     def get_current_game():  # pyright: ignore[reportUnusedFunction]
-        game_state_dto = game_service.get_active_game(current_user)
+        game_state_dto = game_service.get_active_game(current_user._get_current_object())
         return jsonify(game_state_dto.model_dump()), 200
 
     @app.route("/api/game", methods=["PATCH"])
@@ -33,5 +33,5 @@ def init_game_endpoints(app: Flask):
 
         player_move_dto = PlayerMoveDto(**payload)
 
-        game_state_dto = game_service.make_player_move(current_user, player_move_dto)
+        game_state_dto = game_service.make_player_move(current_user._get_current_object(), player_move_dto)
         return jsonify(game_state_dto.model_dump()), 200
