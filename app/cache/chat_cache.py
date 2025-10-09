444c054192a5f15f5e632d54473a38d1dd35818b
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-10-09 14:56:13 +0200

    Implement chat_cache module

--- app/cache/chat_cache.py
+++ app/cache/chat_cache.py
@@ -1,2 +1,39 @@
+from typing import cast
+from uuid import UUID
+from app.cache import redis
 
 
+def add_message_to_chat(match_id: UUID, message: tuple[str, str]):
+    chat_key = _get_key_from_match_id(match_id)
+    redis.rpush(chat_key, _create_str_from_message(message))
+
+
+def get_chat_by_match_id_from_cache(match_id: UUID) -> list[tuple[str, str]]:
+    chat_key = _get_key_from_match_id(match_id)
+    messages: list[bytes] = cast(list[bytes], redis.lrange(chat_key, 0, -1))
+
+    return [_create_message_from_str(msg.decode("utf-8")) for msg in messages]
+
+
+def add_chat_to_cache(match_id: UUID):
+    chat_key = _get_key_from_match_id(match_id)
+    redis.rpush(chat_key, "__init__")
+    redis.lpop(chat_key)
+
+
+def remove_chat_from_cache(match_id: UUID):
+    chat_key = _get_key_from_match_id(match_id)
+    redis.delete(chat_key)
+
+
+def _create_str_from_message(message: tuple[str, str]) -> str:
+    return f"{message[0]}||{message[1]}"
+
+
+def _create_message_from_str(string: str) -> tuple[str, str]:
+    msg_list = string.split("||")
+    return (msg_list[0], msg_list[1])
+
+
+def _get_key_from_match_id(match_id: UUID) -> str:
+    return f"chat_{match_id}"
--- app/service/chat_service.py
+++ app/service/chat_service.py
@@ -1,4 +1,5 @@
 from uuid import UUID
+from app.database.db_models import User
 from app.dto.chat_dto import ChatDto
 
 
@@ -7,4 +8,12 @@ def add_message(user_id: int, message: tuple[str, str]) -> tuple[UUID, ChatDto]:
 
 
 def get_chat_dto_by_user_id(user_id: int) -> ChatDto:
+    pass
+
+
+def add_user_to_chat(match_id: UUID, user: User):
+    pass
+
+
+def remove_user_from_chat(user: User):
     pass
